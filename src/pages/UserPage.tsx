2ee677fb6c61056688e991ec94e419358fec66b4
sona678 <sonamaharjan5@gmail.com>
2025-08-11 11:22:00 +0545

    changes made for menuitem

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -2,6 +2,12 @@ import img1 from '../img/img1.png'
 import { MailOutlined, HeartOutlined, UnorderedListOutlined, HeatMapOutlined } from '@ant-design/icons'
 
 const UserPage = () => {
+  const menuItems = [
+    { icon: <HeartOutlined />, label: 'My Favourites' },
+    { icon: <UnorderedListOutlined />, label: 'Order History' },
+    { icon: <HeatMapOutlined />, label: 'Manage Delivery Address' },
+    { icon: <HeartOutlined />, label: 'Voucher Vault' },
+  ]
   return (
     <div className="mt-4 flex flex-col items-center justify-center bg-slate-100 py-2 text-center">
       <img src={img1} className="center inline-flex h-20 rounded-full border-2 object-cover" alt="profile" />
@@ -12,22 +18,12 @@ const UserPage = () => {
       </p>
       <button className="rounded-3xl bg-yellow-500 px-6 py-2 text-sm hover:bg-yellow-400">EDIT PROFILE</button>
       <div className="py-4">
-        <p className="flex items-center gap-2 font-serif">
-          <HeartOutlined />
-          My Favourites
-        </p>
-        <p className="flex items-center gap-2 font-serif">
-          <UnorderedListOutlined />
-          Order History
-        </p>
-        <p className="flex items-center gap-2 font-serif">
-          <HeatMapOutlined />
-          Manage Delivery Address
-        </p>
-        <p className="flex items-center gap-2 font-serif">
-          <HeartOutlined />
-          Voucher Vault
-        </p>
+        {menuItems.map((item, index) => (
+          <p key={index} className="flex cursor-pointer items-center gap-2 py-2 text-lg hover:bg-gray-200">
+            {item.icon}
+            {item.label}
+          </p>
+        ))}
       </div>
     </div>
   )
